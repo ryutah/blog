dbf674ad0d9e93277834a689b9617eb361f3081d
ryutah <ryuta0221@gmail.com>
2020-07-19 02:08:12 +0900

    fix(script): ファイル名が固定になっていたので修正

--- script/new_post.sh
+++ script/new_post.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)/..
 echo Input new post file name
 read file_name
 
-docker run --rm -it -v $(pwd):/src klakegg/hugo:0.74.2-ext-alpine new --kind posts posts/example
+docker run --rm -it -v $(pwd):/src klakegg/hugo:0.74.2-ext-alpine new --kind posts posts/${file_name}
