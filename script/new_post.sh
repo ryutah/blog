43ae43d5ffbbd9cc8e077a5c33d5f447f9a0ff07
ryutah <ryuta0221@gmail.com>
2020-07-18 23:56:32 +0900

    feat: タスクランナーとscriptを追加

---
+++ script/new_post.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)/..
+
+echo Input new post file name
+read file_name
+
+docker run --rm -it -v $(pwd):/src klakegg/hugo:0.74.2-ext-alpine new --kind posts posts/example
